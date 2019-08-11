562f2637e49196ea60bbcbd5b4b4bd5bedc90ca8
miltt <inbox@miltt.ru>
2019-08-11 21:19:06 +0200

    updated travelling salesman problem

--- Simulated annealing/Travelling Salesman Problem/Program.cs
+++ Simulated annealing/Travelling Salesman Problem/Program.cs
@@ -1,74 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace TravellingSalesmanProblem
 {
+    public struct SimulatedAnnealingResult
+    {
+        public int[] State;
+        public double Energy;
+    }
+
     public sealed class SimulatedAnnealing
     {
-        private const int MaxCycles = 10000;
-        private const double CoefTemperature = 0.1;
+        private const int DefaultIterationsCount = 10000;
+        private const double TemperatureDecreaseRatio = 0.1;
         
-        private Random _random = new Random();
+        private readonly Random _random = new Random();
 
-        public int[] State { get; private set; }
-        public double Energy { get; private set; }
-
-        public void Start(List<City> cities, double startTemperature, double endTemperature)
+        public SimulatedAnnealingResult Start(City[] cities, double startTemperature, double endTemperature)
         {
-            State = Helper.CreateArray(cities.Count).OrderBy(x => _random.Next()).ToArray(); // init random state
+            if (cities == null || cities.Length == 0)
+                throw new ArgumentException("Must contain at least one element", nameof(cities));
+            if (startTemperature < endTemperature)
+                throw new ArgumentException("Must be less then end startTemperature", nameof(endTemperature));
 
-            var temperature = startTemperature;
-            Energy = CalculateEnergy(State, cities); // calculated energy for the first state
+            var result = new SimulatedAnnealingResult();
+            result.State = CreateArray(cities.Length);
+            result.Energy = CalculateEnergy(result.State, cities);
+            
+            var temperature = startTemperature;            
 
-            for (var i = 1; i < MaxCycles; i++)
+            for (var i = 1; i < DefaultIterationsCount; i++)
             {
-                GenerateStateCandidate(State); // prepare the state of candidate
+                GenerateStateCandidate(result.State);
 
-                var candidateState = State;
-                var candidateEnergy = CalculateEnergy(candidateState, cities); // calculated candidate state energy    
+                var candidateState = result.State;
+                var candidateEnergy = CalculateEnergy(candidateState, cities);
 
-                if (candidateEnergy < Energy) // the candidate has less energy
+                if (candidateEnergy < result.Energy || 
+                    MakeTransit(GetTransitionProbability(candidateEnergy - result.Energy, temperature)))
                 {
-                    Energy = candidateEnergy; // it goes into the current state
-                    State = candidateState;
-                }
-                else
-                {
-                    var probability = GetTransitionProbability(candidateEnergy - Energy, temperature); // otherwise, calculate the probability
-                    if (MakeTransit(probability)) // and look, are there transition
-                    {
-                        Energy = candidateEnergy;
-                        State = candidateState;
-                    }
+                    result.Energy = candidateEnergy;
+                    result.State = candidateState;
                 }
 
-                temperature = startTemperature * CoefTemperature / i; // temperature reduction
-                if (temperature <= endTemperature) // check exit conditions
+                temperature = DecreaseTemperature(startTemperature, i);
+                if (temperature <= endTemperature)
                     break;
             }
+
+            return result;
         }
 
-        private double CalculateEnergy(int[] state, List<City> cities)
+        private double CalculateEnergy(int[] state, City[] cities)
         {
             var energy = 0.0;
-            for (var i = 0; i < state.Count() - 1; i++)            
-                energy += GetEuclideanDistance(cities.ElementAt(state[i]), cities.ElementAt(state[i + 1]));            
+            for (int i = 0; i < state.Length - 1; i++)            
+                energy += GetEuclideanDistance(cities[state[i]], cities[state[i + 1]]);
 
-            energy += GetEuclideanDistance(cities.ElementAt(state[state.Count() - 1]), cities.ElementAt(state[0])); // close the route
+            energy += GetEuclideanDistance(cities[state[state.Length - 1]], cities[state[0]]); // close the route
             return energy;
         }
 
-        private double GetEuclideanDistance(City cityFrom, City cityTo)
-        {
-            return Math.Sqrt(Math.Pow(cityFrom.X - cityTo.X, 2) + Math.Pow(cityFrom.Y - cityTo.Y, 2));
-        }
+        private double GetEuclideanDistance(City from, City to)
+            => Math.Sqrt(Math.Pow(from.X - to.X, 2) + Math.Pow(from.Y - to.Y, 2));
 
         private void GenerateStateCandidate(int[] state)
         {
-            var i = _random.Next(0, state.Count());
-            var j = _random.Next(0, state.Count());
+            var i = _random.Next(0, state.Length);
+            var j = _random.Next(0, state.Length);
             if (j < i)
                 state.Swap(j, i);
             else
@@ -76,16 +74,24 @@ namespace TravellingSalesmanProblem
         }
 
         private double GetTransitionProbability(double energy, double temperature)
-        {
-            return Math.Exp(-energy / temperature);
-        }
+            => Math.Exp(-energy / temperature);
 
         private bool MakeTransit(double probability)
-        {
-            if (probability > 1 || probability < 0)
-                throw new ArgumentException(nameof(probability));
+            => probability > 1 || probability < 0 ? false : _random.NextDouble() <= probability;
 
-            return (_random.Next(0, 1) <= probability);
+        private double DecreaseTemperature(double temperature, int i)
+            => i > 0 ? temperature * TemperatureDecreaseRatio / i : temperature;
+
+        private int[] CreateArray(int length)
+        {
+            var array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = i;            
+            
+            for (int i = 0; i < array.Length; i++)
+                array.Swap(i, _random.Next(array.Length));
+
+            return array;
         }
     }
 
@@ -93,59 +99,45 @@ namespace TravellingSalesmanProblem
     {
         static void Main(string[] args)
         {
-            var cities = new List<City>
+            var cities = new City[]
             {
-                new City { X = 500, Y = 500 },
-                new City { X = 2000, Y = 250 },
-                new City { X = 1000, Y = 2500 },
-                new City { X = 1500, Y = 1200 },
-                new City { X = 2465, Y = 1779 }
+                new City(500, 500),
+                new City(2000, 250),
+                new City(1000, 2500),
+                new City(1500, 1200),
+                new City(2465, 1779)
             };
-            var startTemperature = 100;
-            var endTemperature = 0.000001;
 
-            var annealing = new SimulatedAnnealing();
-            annealing.Start(cities, startTemperature, endTemperature);
+            var result = new SimulatedAnnealing().Start(cities, startTemperature: 100, endTemperature: 0.000001);
+            
+            Console.Write($"Energy: {result.Energy}, Route: ");        
+            for (int i = 0; i < result.State.Length; i++)
+                Console.Write($"{result.State[i]} ");
 
-            var sb = new StringBuilder();
-            annealing.State.ForEach(s => sb.Append(s));
-
-            Console.WriteLine($"Route: {sb.ToString()}. Energy: {annealing.Energy}");
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
-    }
+    }    
 
-    public class City
+    public struct City
     {
-        public int X { get; set; }
-        public int Y { get; set; }
+        public int X;
+        public int Y;
+        
+        public City(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 
-    public static class Helper
+    public static class Extension
     {
         public static void Swap(this int[] array, int i, int j)
         {
             var temp = array[i];
             array[i] = array[j];
             array[j] = temp;
-        }
-
-        public static int[] CreateArray(int length)
-        {
-            var array = new int[length];
-            for (var i = 0; i < array.Length; i++)
-                array[i] = i;            
-            return array;
-        }
-
-        public static void ForEach<T>(this T[] items, Action<T> action)
-        {
-            if (items == null || !items.Any())
-                return;
-
-            foreach (var item in items)
-                action?.Invoke(item);
         }
     }
 }
