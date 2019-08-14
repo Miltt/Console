0ad7dc43853aaccde31af826462ca380bb66a34a
miltt <inbox@miltt.ru>
2019-08-14 02:17:07 +0200

    lcs problem updated

--- Dynamic programming/Longest common subsequence problem/Program.cs
+++ Dynamic programming/Longest common subsequence problem/Program.cs
@@ -1,44 +1,44 @@
 ﻿using System;
 
-namespace LongestCommonSubsequenceProblem
+namespace LCS
 {
     public class LongestCommonSubsequence
     {
-        private int[,] _result;
-
-        public void Solve(char[] a, char[] b)
+        public static int[,] Find(char[] a, char[] b)
         {
-            Calc(a, b);
-            Console.WriteLine(GetBackTrack(a, b, a.Length - 1, b.Length - 1));
-        }
+            if (a is null)
+                throw new ArgumentNullException(nameof(a));
+            if (b is null)
+                throw new ArgumentNullException(nameof(b));
 
-        private void Calc(char[] a, char[] b)
-        {
-            _result = new int[a.Length, b.Length];
+            var result = new int[a.Length, b.Length];
 
-            for (var i = 1; i < a.Length; i++)
+            for (int i = 1; i < a.Length; i++)
             {
-                for (var j = 1; j < b.Length; j++)
+                for (int j = 1; j < b.Length; j++)
                 {
-                    _result[i, j] = a[i] == b[j]
-                        ? _result[i - 1, j - 1] + 1
-                        : Math.Max(_result[i, j - 1], _result[i - 1, j]);
+                    result[i, j] = a[i] == b[j]
+                        ? result[i - 1, j - 1] + 1
+                        : Math.Max(result[i, j - 1], result[i - 1, j]);
                 }
             }
+
+            return result;
         }
 
-        private string GetBackTrack(char[] a, char[] b, int i, int j)
-        {
+        public static string GetBackTrack(int[,] result, char[] a, char[] b, int i, int j)
+        {        
+            if (result is null || a is null || b is null)
+                throw new ArgumentException("Args cannot be null");
+                
             if (i == 0 || j == 0)
                 return string.Empty;
-
             if (a[i] == b[j])
-                return GetBackTrack(a, b, i - 1, j - 1) + a[i];
+                return GetBackTrack(result, a, b, i - 1, j - 1) + a[i];
+            if (result[i, j - 1] > result[i - 1, j])
+                return GetBackTrack(result, a, b, i, j - 1);
             
-            if (_result[i, j - 1] > _result[i - 1, j])
-                return GetBackTrack(a, b, i, j - 1);
-            
-            return GetBackTrack(a, b, i - 1, j);
+            return GetBackTrack(result, a, b, i - 1, j);
         }
     }
 
@@ -49,12 +49,12 @@ namespace LongestCommonSubsequenceProblem
             var a = "abcbat".ToCharArray();
             var b = "dcbeta".ToCharArray();
 
-            var longestSubsequence = new LongestCommonSubsequence();
-            longestSubsequence.Solve(a, b);
+            var result = LongestCommonSubsequence.Find(a, b);
+            var backTrack = LongestCommonSubsequence.GetBackTrack(result, a, b, a.Length - 1, b.Length - 1);
+            Console.WriteLine(backTrack);
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
     }
 }
-
