cfa953efe7d5a268060832f412598a8644f53db2
miltt <inbox@miltt.ru>
2019-08-31 17:29:54 +0200

    update avl tree

--- Data structure/AVL tree/Program.cs
+++ Data structure/AVL tree/Program.cs
@@ -4,7 +4,7 @@ namespace AVLTree
 {
     public class Node
     {
-        public int Key { get; set; }
+        public int Key { get; }
         public int Height { get; set; }
         public Node Left { get; set; }
         public Node Right { get; set; }
@@ -19,29 +19,26 @@ namespace AVLTree
     public class Avl
     {
         private const int DefaultBalance = 2;
-        private const int DefaultRootKey = 4;
 
-        private Node _root;
-        private IPerformer _performer;
+        public Node Root { get; private set; }
 
-        public Avl(IPerformer performer) 
-            : this(performer, DefaultRootKey) { }
+        public void Add(int key)
+            => Add(Root, key);
 
-        public Avl(IPerformer performer, int rootKey)
-        {
-            _root = new Node(rootKey);
-            _performer = performer;
-        }
-
-        public Node Add(int key)
-        {
-            return Add(_root, key);
-        }
+        public void Remove(int key)
+            => Remove(Root, key);
 
         private Node Add(Node node, int key)
         {
             if (node == null)
-                return new Node(key);
+            {
+                node = new Node(key);
+
+                if (Root == null)
+                    Root = node;
+
+                return node;
+            }
 
             if (key < node.Key)
                 node.Left = Add(node.Left, key);
@@ -49,11 +46,6 @@ namespace AVLTree
                 node.Right = Add(node.Right, key);
 
             return Balancing(node);
-        }
-
-        public Node Remove(int key)
-        {
-            return Remove(_root, key);
         }
 
         private Node Remove(Node node, int key)
@@ -85,66 +77,10 @@ namespace AVLTree
             return Balancing(node);
         }
 
-        public void PreOrderTravers()
-        {
-            PreOrderTravers(_root);
-        }
-
-        private void PreOrderTravers(Node root)
-        {
-            if (root != null)
-            {
-                _performer.Perform(root.Key);
-                PreOrderTravers(root.Left);
-                PreOrderTravers(root.Right);
-            }
-        }
-
-        public void InOrderTravers()
-        {
-            InOrderTravers(_root);
-        }
-
-        private void InOrderTravers(Node root)
-        {
-            if (root != null)
-            {
-                InOrderTravers(root.Left);
-                _performer.Perform(root.Key);
-                InOrderTravers(root.Right);
-            }
-        }
-
-        public void PostOrderTravers()
-        {
-            PostOrderTravers(_root);
-        }
-
-        private void PostOrderTravers(Node root)
-        {
-            if (root != null)
-            {
-                PostOrderTravers(root.Left);
-                PostOrderTravers(root.Right);
-                _performer.Perform(root.Key);
-            }
-        }
-
-        private int GetHeight(Node node)
-        {
-            return node?.Height ?? 0;
-        }
-
-        private int GetBalance(Node node)
-        {
-            return GetHeight(node.Right) - GetHeight(node.Left);
-        }
-
         private void RecalcHeight(Node node)
         {
             var hLeft = GetHeight(node.Left);
             var hRight = GetHeight(node.Right);
-
             node.Height = (hLeft > hRight ? hLeft : hRight) + 1;
         }
 
@@ -195,11 +131,6 @@ namespace AVLTree
             return node;
         }
 
-        private Node FindMin(Node node)
-        {
-            return node.Left != null ? FindMin(node.Left) : node;
-        }
-
         private Node RemoveMin(Node node)
         {
             if (node.Left == null)
@@ -208,20 +139,27 @@ namespace AVLTree
             node.Left = RemoveMin(node.Left);
             return Balancing(node);
         }
+
+        private Node FindMin(Node node) => node.Left != null ? FindMin(node.Left) : node;
+        
+        private int GetHeight(Node node) => node?.Height ?? 0;
+
+        private int GetBalance(Node node) => GetHeight(node.Right) - GetHeight(node.Left);
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            var avlTree = new Avl(new Display());
+            var avlTree = new Avl();
+            avlTree.Add(4);
             avlTree.Add(3);
             avlTree.Add(5);
             avlTree.Add(7);
             avlTree.Add(1);
             avlTree.Add(6);
             
-            avlTree.InOrderTravers();
+            AvlTreeTravers.InOrderTravers(avlTree.Root);
             
             avlTree.Remove(3);
             avlTree.Remove(6);
@@ -231,6 +169,41 @@ namespace AVLTree
         }
     }
 
+    public static class AvlTreeTravers
+    {
+        private static readonly IPerformer Performer = new Display();
+
+        public static void PreOrderTravers(Node root)
+        {
+            if (root != null)
+            {
+                Performer.Perform(root.Key);
+                PreOrderTravers(root.Left);
+                PreOrderTravers(root.Right);
+            }
+        }
+
+        public static void InOrderTravers(Node root)
+        {
+            if (root != null)
+            {
+                InOrderTravers(root.Left);
+                Performer.Perform(root.Key);
+                InOrderTravers(root.Right);
+            }
+        }
+
+        public static void PostOrderTravers(Node root)
+        {
+            if (root != null)
+            {
+                PostOrderTravers(root.Left);
+                PostOrderTravers(root.Right);
+                Performer.Perform(root.Key);
+            }
+        }
+    }    
+
     public interface IPerformer
     {
         void Perform(int data);
@@ -238,9 +211,6 @@ namespace AVLTree
 
     public class Display : IPerformer
     {
-        public void Perform(int data)
-        {
-            Console.Write($"{data} ");
-        }
+        public void Perform(int data) => Console.Write($"{data} ");
     }
 }
