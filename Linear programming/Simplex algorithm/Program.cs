8a8e49e546337762059c35bf1aac639cc280632a
miltt <inbox@miltt.ru>
2019-09-06 02:45:17 +0200

    update simplex

--- Linear programming/Simplex algorithm/Program.cs
+++ Linear programming/Simplex algorithm/Program.cs
@@ -3,117 +3,134 @@ using System.Text;
 
 namespace LinearProgramming
 {
-    public sealed class Simplex
+    public struct Result
     {
-        private const int RowsDim = 0;
-        private const int ColumnsDim = 1;
+        public double[,] Table;
+        public bool[] BasicSolution;
 
-        private double[] _funcCoefficients;
-        private double[,] _bounds;
-        private double[,] _table;
-        private bool[] _basicSolution;
-        private IPerformer _performer;
-
-        public Simplex(double[] funcCoefficients, double[,] bounds, IPerformer performer)
+        public Result(double[,] table, bool[] basicSolution)
         {
-            _funcCoefficients = funcCoefficients;
-            _bounds = bounds;
-            _performer = performer;
+            Table = table;
+            BasicSolution = basicSolution;
         }
 
-        public void Maximize()
+        public string GetSolution()
         {
-            Init();
+            var sb = new StringBuilder();
 
-            while (!IsOptimalSolution())
+            for (int i = 0; i < BasicSolution.Length; i++)
             {
-                double pivotValue;
-                int pivotRow;
-                int pivotColumn;
-                PreparePivot(out pivotValue, out pivotRow, out pivotColumn);
+                if (BasicSolution[i])
+                    sb.Append($"{Table[i, Table.GetUpperBound(1)]} ");
+            }
+
+            return sb.ToString();
+        } 
+    }
+
+    public sealed class Simplex
+    {
+        private struct Pivot
+        {
+            public double Value;
+            public int Row;
+            public int Column;
+        }
+
+        private const int RowDimension = 0;
+        private const int ColumnDimensions = 1;
+
+        public Result Maximize(double[] funcCoefficients, double[,] bounds)
+        {
+            var result = Init(funcCoefficients, bounds);
+
+            while (!IsOptimalSolution(funcCoefficients, result.Table))
+            {
+                var pivot = GetPivot(funcCoefficients, result);
 
                 // create new table and calc all values except the pivot row and column
-                var tempTable = new double[_table.GetLength(RowsDim), _table.GetLength(ColumnsDim)];
-                for (int i = 0; i < tempTable.GetLength(RowsDim); i++)
+                var tempTable = new double[result.Table.GetLength(RowDimension), result.Table.GetLength(ColumnDimensions)];
+                for (int i = 0; i < tempTable.GetLength(RowDimension); i++)
                 {
-                    for (int j = 0; j < tempTable.GetLength(ColumnsDim); j++)
-                        tempTable[i, j] = (pivotValue * _table[i, j] - _table[pivotRow, j] * _table[i, pivotColumn]) / pivotValue;
+                    for (int j = 0; j < tempTable.GetLength(ColumnDimensions); j++)
+                        tempTable[i, j] = (pivot.Value * result.Table[i, j] - result.Table[pivot.Row, j] * result.Table[i, pivot.Column]) / pivot.Value;
                 }
 
                 // calc of the remaining values of the pivot element
-                for (int i = 0; i < _table.GetLength(ColumnsDim); i++)
-                    tempTable[pivotRow, i] = _table[pivotRow, i] / pivotValue;
+                for (int i = 0; i < result.Table.GetLength(ColumnDimensions); i++)
+                    tempTable[pivot.Row, i] = result.Table[pivot.Row, i] / pivot.Value;
+                for (int i = 0; i < result.Table.GetLength(RowDimension); i++)
+                    tempTable[i, pivot.Column] = (result.Table[i, pivot.Column] / pivot.Value) * -1;
 
-                for (int i = 0; i < _table.GetLength(RowsDim); i++)
-                    tempTable[i, pivotColumn] = (_table[i, pivotColumn] / pivotValue) * -1;
-
-                tempTable[pivotRow, pivotColumn] = 1 / pivotValue;
-                _table = tempTable;
+                tempTable[pivot.Row, pivot.Column] = 1 / pivot.Value;
+                result.Table = tempTable;
             }
 
-            _performer.Perform(_table, _basicSolution);
+            return result;
         }
 
-        private void Init()
+        private Result Init(double[] funcCoefficients, double[,] bounds)
         {
-            _table = new double[_bounds.GetLength(RowsDim) + 1, _funcCoefficients.Length + 1];
-            _basicSolution = new bool[_bounds.GetLength(RowsDim) + 1];
+            var table = new double[bounds.GetLength(RowDimension) + 1, funcCoefficients.Length + 1];
 
-            for (int i = 0; i < _bounds.GetLength(RowsDim); i++)
+            for (int i = 0; i < bounds.GetLength(RowDimension); i++)
             {
-                for (int j = 0; j < _bounds.GetLength(ColumnsDim); j++)
-                    _table[i, j] = _bounds[i, j];
+                for (int j = 0; j < bounds.GetLength(ColumnDimensions); j++)
+                    table[i, j] = bounds[i, j];
             }
 
-            for (int i = 0; i < _funcCoefficients.Length; i++)
-                _table[_table.GetUpperBound(RowsDim), i] = _funcCoefficients[i] * -1;
+            for (int i = 0; i < funcCoefficients.Length; i++)
+                table[table.GetUpperBound(RowDimension), i] = funcCoefficients[i] * -1;
+
+            return new Result(table, new bool[bounds.GetLength(RowDimension) + 1]);
         }
 
-        private bool IsOptimalSolution()
+        private bool IsOptimalSolution(double[] funcCoefficients, double[,] table)
         {
-            for (int i = 0; i < _funcCoefficients.Length; i++)
+            for (int i = 0; i < funcCoefficients.Length; i++)
             {
-                if (_table[_table.GetUpperBound(RowsDim), i] < 0)
+                if (table[table.GetUpperBound(RowDimension), i] < 0)
                     return false;
             }
 
             return true;
         }
 
-        private void PreparePivot(out double pivotValue, out int row, out int column)
+        private Pivot GetPivot(double[] funcCoefficients, Result result)
         {
-            column = int.MinValue;
-            row = int.MinValue;
+            var pivot = new Pivot();
 
             var min = double.MaxValue;
-            for (int i = 0; i < _funcCoefficients.Length; i++)
+            for (int i = 0; i < funcCoefficients.Length; i++)
             {
-                if (min > _table[_table.GetUpperBound(RowsDim), i])
+                if (min > result.Table[result.Table.GetUpperBound(RowDimension), i])
                 {
-                    min = _table[_table.GetUpperBound(RowsDim), i];
-                    column = i;
+                    min = result.Table[result.Table.GetUpperBound(RowDimension), i];
+                    pivot.Column = i;
                 }
             }
 
             min = double.MaxValue;
-            for (int i = 0; i < _table.GetLength(RowsDim); i++)
+            for (int i = 0; i < result.Table.GetLength(RowDimension); i++)
             {
-                if (_table[i, column] > 0)
+                if (result.Table[i, pivot.Column] > 0)
                 {
-                    var temp = _table[i, _table.GetUpperBound(ColumnsDim)] / _table[i, column];
+                    var temp = result.Table[i, result.Table.GetUpperBound(ColumnDimensions)] / result.Table[i, pivot.Column];
                     if (min > temp)
                     {
                         min = temp;
-                        row = i;
+                        pivot.Row = i;
                     }
                 }
             }
 
-            if (row == int.MinValue || column == int.MinValue)
-                throw new Exception("There is no solution");
+            if (pivot.Row == int.MinValue || pivot.Column == int.MinValue)
+                throw new InvalidOperationException("There is no solution");
 
-            pivotValue = _table[row, column];
-            _basicSolution[row] = true; 
+            pivot.Value = result.Table[pivot.Row, pivot.Column];
+            result.BasicSolution[pivot.Row] = true; 
+
+            return pivot;
         }
     }
 
@@ -130,32 +147,11 @@ namespace LinearProgramming
             // 2x1 + x2 + u3 = 6
             var bounds = new double[,] { { 1, -2, 1 }, { -2, 1, 2 }, { 2, 1, 6} };
 
-            var simplex = new Simplex(funcCoefficients, bounds, new Performer());
-            simplex.Maximize();
+            var result = new Simplex().Maximize(funcCoefficients, bounds);
+            Console.WriteLine(result.GetSolution());
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
-        }
-    }
-
-    public interface IPerformer
-    {
-        void Perform(double[,] _table, bool[] _basicSolution);
-    }
-
-    public class Performer : IPerformer
-    {
-        private StringBuilder _sb = new StringBuilder();
-
-        public void Perform(double[,] _table, bool[] _basicSolution)
-        {
-            for (int i = 0; i < _basicSolution.Length; i++)
-            {
-                if (_basicSolution[i])
-                    _sb.Append($"{_table[i, _table.GetUpperBound(1)]} ");
-            }
-
-            Console.WriteLine(_sb);
         }
     }
 }
