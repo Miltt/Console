236fff73377ae600c65d027a21a8822a7ac82797
miltt <inbox@miltt.ru>
2019-09-11 02:21:38 +0200

    update eight queens puzzle

--- Miscellaneous/EightQueensPuzzle/Program.cs
+++ Miscellaneous/EightQueensPuzzle/Program.cs
@@ -1,61 +1,88 @@
 ﻿using System;
+using System.Text;
 
 namespace EQP
 {
-    public class EightQueensPuzzle
+    public struct Сhessboard
     {
-        private const int N = 8;
+        public bool[,] Grid;
+        public int Size;
 
-        public void Solve()
+        public Сhessboard(int size)
         {
-            var board = new bool[N, N];
-            if (CanFind(board, 0))
-                Show(board);
+            Grid = new bool[size, size];
+            Size = size;
         }
 
-        private bool Allowed(bool[,] board, int x, int y)
+        public string GetResult()
         {
-            for (int i = 0; i <= x; i++)
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < Size; i++)
             {
-                if (board[i, y] || (i <= y && board[x - i, y - i]) || (y + i < N && board[x - i, y + i]))
-                    return false;
+                for (int j = 0; j < Size; j++)
+                {
+                    if (Grid[i, j])
+                        sb.Append(i).Append(':').Append(j).Append(' ');
+                }
             }
 
-            return true;
+            return sb.ToString();
+        }
+    }
+
+    public sealed class EightQueensPuzzle
+    {
+        public Сhessboard Run(int boardSize)
+        {
+            if (boardSize <= 0)
+                throw new ArgumentException("Must be less than 0", nameof(boardSize));
+
+            var board = new Сhessboard(boardSize);
+            
+            TryFind(board, 0);
+
+            return board;
         }
 
-        private bool CanFind(bool[,] board, int x)
+        private bool TryFind(in Сhessboard board, int i)
         {
-            for (var y = 0; y < N; y++)
+            for (int j = 0; j < board.Size; j++)
             {
-                if (Allowed(board, x, y))
+                if (IsValid(board, i, j))
                 {
-                    board[x, y] = true;
-                    if (x == N - 1 || CanFind(board, x + 1))
+                    board.Grid[i, j] = true;
+                    
+                    if (i == board.Size - 1 || TryFind(board, i + 1))
                         return true;
 
-                    board[x, y] = false;
+                    board.Grid[i, j] = false;
                 }
             }
 
             return false;
         }
 
-        private void Show(bool[,] board)
+        private bool IsValid(in Сhessboard board, int i, int j)
         {
-            for (int i = 0; i < N; i++)
+            for (int k = 0; k <= i; k++)
             {
-                for (int j = 0; j < N; j++)
-                    Console.Write(board[i, j] ? i + ":" + j + " " : "");
+                if ((board.Grid[k, j]) || 
+                    (k <= j && board.Grid[i - k, j - k]) || 
+                    (j + k < board.Size && board.Grid[i - k, j + k]))
+                    return false;
             }
-        }
+
+            return true;
+        }        
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            new EightQueensPuzzle().Solve();
+            var board = new EightQueensPuzzle().Run(boardSize: 8);
+            Console.WriteLine(board.GetResult());
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
