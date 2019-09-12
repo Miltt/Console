7d3ffee65dbbb1b7c3e4b0ca18e77714393e199b
miltt <inbox@miltt.ru>
2019-09-12 02:06:18 +0200

    update hanoi tower

--- Miscellaneous/Hanoi/Program.cs
+++ Miscellaneous/Hanoi/Program.cs
@@ -2,52 +2,30 @@
 
 namespace Hanoi
 {
-    public class TowerOfHanoi
+    public static class TowerOfHanoi
     {
-        private IPerformer _performer;
-
-        public TowerOfHanoi(IPerformer performer)
+        public static void Solve(int diskCount, int from, int to, int buffer)
         {
-            _performer = performer;
-        }
-
-        public void Play(int numDisks, int from, int to, int buffer)
-        {
-            if (numDisks >= 1)
+            if (diskCount >= 1)
             {
-                Play(numDisks - 1, from, buffer, to);
+                Solve(diskCount - 1, from, buffer, to);
                 MoveDisk(from, to);
-                Play(numDisks - 1, buffer, to, from);
+                Solve(diskCount - 1, buffer, to, from);
             }
         }
 
-        private void MoveDisk(int from, int to)
-        {
-            _performer.Perform(from, to);
-        }
+        private static void MoveDisk(int from, int to)
+            => Console.WriteLine($"Move top disc from {from} to {to}");
     }
 
     class Program
     {        
         static void Main(string[] args)
         {
-            new TowerOfHanoi(new Performer()).Play(3, 1, 3, 2);
+            TowerOfHanoi.Solve(3, 1, 3, 2);
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
-        }
-    }
-
-    public interface IPerformer
-    {
-        void Perform(int from, int to);
-    }
-
-    public class Performer : IPerformer
-    {
-        public void Perform(int from, int to)
-        {
-            Console.WriteLine($"Move top disc from {from} to {to}");
         }
     }
 }
