b4e86b3fc980317105e1faa54fea4cec3d91d968
miltt <inbox@miltt.ru>
2019-09-22 21:07:01 +0200

    update adjacency matrix

--- Graph/AdjacencyMatrix/Program.cs
+++ Graph/AdjacencyMatrix/Program.cs
@@ -5,53 +5,155 @@ using System.Text;
 
 namespace AdjacencyMatrix
 {
-    public class Item
+    public class Graph
     {
-        public string Name { get; set; }
-        public List<Arc> Arcs { get; set; } = new List<Arc>();
+        private Vertex[] _vertices;
 
-        public void AddArc(Item child, int weigth)
+        public int Count => _vertices.Length;
+
+        public Graph(int verticesCount)
         {
-            Arcs.Add(new Arc { Weigth = weigth, Parent = this, Child = child });                
+            if (verticesCount < 0)
+                throw new ArgumentException("Must be at least 0", nameof(verticesCount));
 
-            if (!child.Arcs.Exists(a => a.Parent == child && a.Child == this))
-                child.AddArc(this, weigth);
+            _vertices = new Vertex[verticesCount];
+            
+            for (int i = 0; i < _vertices.Length; i++)
+		        _vertices[i] = new Vertex($"{(char)('A' + i)}");
+        }
+
+        public void AddEdge(int parent, int child, int weight)
+        {
+            var parentVertex = GetVertex(parent);
+            var childVertex = GetVertex(child);
+            if (!parentVertex.Equals(default) && !childVertex.Equals(default))
+                parentVertex.AddEdge(childVertex, weight);
+        }
+
+        public Vertex GetVertex(int i)
+        {
+            var vertex = _vertices[i];
+            if (vertex == null)
+                throw new InvalidOperationException($"Vertex not found by index {i}");
+            return vertex;
         }
     }
 
-    public class Arc
+    public class Vertex
     {
-        public int Weigth { get; set; }
-        public Item Parent { get; set; }
-        public Item Child { get; set; }
+        private List<Edge> _edges = new List<Edge>();
+
+        public string Name { get; }
+        public IReadOnlyCollection<Edge> Edges => _edges;
+
+        public Vertex(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public void AddEdge(Vertex child, int weight)
+        {
+            if (child is null)
+                throw new ArgumentNullException(nameof(child));
+            if (weight < 0)
+                throw new ArgumentException("Must be at least 0", nameof(weight));
+
+            _edges.Add(new Edge(weight, this, child));                
+
+            if (!child.Edges.Any(a => ReferenceEquals(a.Parent, child) && ReferenceEquals(a.Child, this)))
+                child.AddEdge(this, weight);
+        }
+    }
+
+    public readonly struct Edge : IEquatable<Edge>
+    {
+        public int Weight { get; }
+        public Vertex Parent { get; }
+        public Vertex Child { get; }
+
+        public Edge(int weight, Vertex parent, Vertex child)
+        {
+            if (weight < 0)
+                throw new ArgumentException("Must be at least 0", nameof(weight));
+
+            Weight = weight;
+            Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+            Child = child ?? throw new ArgumentNullException(nameof(child));
+        }
+
+        public bool Equals(Edge other)
+        {
+            return Weight == other.Weight
+                && ReferenceEquals(Parent, other.Parent)
+                && ReferenceEquals(Child, other.Child);
+        }
     }
 
     public class AdjacencMatrix
     {
-        private IPerformer _performer;
-
-        public AdjacencMatrix(IPerformer performer)
+        public static Matrix Convert(Graph graph)
         {
-            _performer = performer;
-        }
+            if (graph is null)
+                throw new ArgumentNullException(nameof(graph));
 
-        public void Create(List<Item> items)
-        {
-            var adjacentMatrix = new int?[items.Count, items.Count];
+            var matrix = new Matrix(graph.Count);
 
-            for (var i = 0; i < items.Count; i++)
+            for (int i = 0; i < graph.Count; i++)
             {
-                var parent = items[i];
-                for (var j = 0; j < items.Count; j++)
+                var parent = graph.GetVertex(i);
+                for (int j = 0; j < graph.Count; j++)
                 {
-                    var child = items[j];
-                    var arc = parent.Arcs.FirstOrDefault(a => a.Child == child);
-                    if (arc != null)
-                        adjacentMatrix[i, j] = arc.Weigth;
+                    var child = graph.GetVertex(j);                   
+                    var edge = parent.Edges.FirstOrDefault(e => ReferenceEquals(e.Child, child));
+                    if (!edge.Equals(default))
+                        matrix.Grid[i, j] = edge.Weight;
                 }
             }
 
-            _performer.Perform(adjacentMatrix);
+            return matrix;
+        }
+    }
+
+    public readonly struct Matrix
+    {
+        public readonly int[,] Grid;
+        public readonly int Size;
+
+        public Matrix(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Must be at least 1", nameof(size));
+
+            Grid = new int[size, size];
+            Size = size;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("    ");
+
+            for (int i = 0; i < Size; i++)
+                sb.Append($"{(char)('A' + i)}  ");
+
+            sb.AppendLine();
+
+            for (int i = 0; i < Size; i++)
+            {
+                sb.Append($"{(char)('A' + i)} [");
+
+                for (int j = 0; j < Size; j++)
+                {
+                    if (i == j)
+                        sb.Append(" - ");
+                    else
+                        sb.Append($" {Grid[i, j]} ");
+                }
+
+                sb.Append("]" + Environment.NewLine);
+            }
+
+            return sb.ToString();
         }
     }
 
@@ -59,71 +161,24 @@ namespace AdjacencyMatrix
     {
         static void Main(string[] args)
         {
-            var a = new Item { Name = "A" };
-            var b = new Item { Name = "B" };
-            var c = new Item { Name = "C" };
-            var d = new Item { Name = "D" };
-            var e = new Item { Name = "E" };
-            var f = new Item { Name = "F" };
-            var g = new Item { Name = "G" };
-            var h = new Item { Name = "H" };
-
-            a.AddArc(b, 1);
-            a.AddArc(c, 1);
-            b.AddArc(e, 1);
-            b.AddArc(d, 3);
-            c.AddArc(f, 1);
-            c.AddArc(d, 3);
-            d.AddArc(h, 8);
-            e.AddArc(g, 1);
-            e.AddArc(h, 3);
-            f.AddArc(h, 3);
-
-            new AdjacencMatrix(new Display()).Create(new List<Item> { a, b, c, d, e, f, g, h });
+            var graph = new Graph(verticesCount: 8);
+            graph.AddEdge(parent: 0, child: 0, weight: 0);
+            graph.AddEdge(parent: 0, child: 1, weight: 1);
+            graph.AddEdge(parent: 0, child: 3, weight: 1);
+            graph.AddEdge(parent: 1, child: 4, weight: 1);
+            graph.AddEdge(parent: 1, child: 3, weight: 3);
+            graph.AddEdge(parent: 2, child: 5, weight: 1);
+            graph.AddEdge(parent: 2, child: 3, weight: 3);
+            graph.AddEdge(parent: 3, child: 7, weight: 8);
+            graph.AddEdge(parent: 4, child: 6, weight: 1);
+            graph.AddEdge(parent: 4, child: 7, weight: 3);
+            graph.AddEdge(parent: 5, child: 7, weight: 3);
+ 
+            var matrix = AdjacencMatrix.Convert(graph);
+            Console.WriteLine(matrix);
 
             Console.WriteLine("Press any key...");
             Console.ReadKey();
-        }
-    }
-
-    public interface IPerformer
-    {
-        void Perform(int?[,] matrix);
-    }
-
-    public sealed class Display : IPerformer
-    {
-        private StringBuilder _sb = new StringBuilder();
-
-        public void Perform(int?[,] matrix)
-        {
-            var length = matrix.GetLength(1);
-
-            _sb.Append("    ");
-
-            for (var i = 0; i < length; i++)
-                _sb.Append($"{(char)('A' + i)}  ");
-
-            _sb.AppendLine();
-
-            for (var i = 0; i < length; i++)
-            {
-                _sb.Append($"{(char)('A' + i)} [");
-
-                for (var j = 0; j < length; j++)
-                {
-                    if (i == j)
-                        _sb.Append(" - ");
-                    else if (matrix[i, j] == null)
-                        _sb.Append(" 0 ");
-                    else
-                        _sb.Append($" {matrix[i, j]} ");
-                }
-
-                _sb.Append("]\r\n");
-            }
-
-            Console.WriteLine(_sb);
         }
     }
 }
